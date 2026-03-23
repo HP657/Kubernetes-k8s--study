1349b29ad0a40e74c67c7838049eb04a0ecb4f1f
HP657 <junlee5070@gmail.com>
2026-03-23 12:55:11 +0900

    5. 서버 수정 됐을때

--- five/src/main/java/com/kube/test/IndexController.java
+++ five/src/main/java/com/kube/test/IndexController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
+//    public String index() {
+//        return "Hello, World!";
+//    }
     public String index() {
-        return "Hello, World!";
+        return "Version: 1.1";
     }
 }
